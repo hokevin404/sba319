98612b40b30dd76b6878252a9a14d6800aa71876
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-19 16:51:47 -0700

    initializations

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// Imports 
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// Initializations
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
+mongoose.connect(process.env.ATLAS_URI);
+app.use(express.json());
 
+// Testing for server activity
 app.listen(PORT, (req, res) => {
     console.log(`Server listening on port: ${PORT}`);
 });
