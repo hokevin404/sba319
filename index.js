6700bfb7cc9614be5a6fccad1aea3133ebbee5fa
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-06-19 17:02:58 -0700

    Added root GET

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ dotenv.config();
 mongoose.connect(process.env.ATLAS_URI);
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send(`Welcome to The World`);
+})
+
 // Testing for server activity
 app.listen(PORT, (req, res) => {
     console.log(`Server listening on port: ${PORT}`);
